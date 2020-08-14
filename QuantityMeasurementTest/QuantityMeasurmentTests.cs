204689656661990168d79f915d03edf584a785d4
unknown <jitesh0dubey@gmail.com>
2020-08-14 09:49:58 +0530

    [Jitesh dubey]ADDED:Initial Commit

---
+++ QuantityMeasurementTest/QuantityMeasurmentTests.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="QuantityMeasurmentTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace QuantityMeasurmentTest
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// This class is for testing  QuantityMeasurment.
+    /// </summary>
+    public class QuantityMeasurmentTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+
+}
