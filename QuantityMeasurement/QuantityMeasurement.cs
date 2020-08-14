540c5d9a9e3d78606c936d244747de4d494e5f65
unknown <jitesh0dubey@gmail.com>
2020-08-14 10:52:55 +0530

    [Jitesh dubey]ADDED:Feature for Inch and Feet Equality

--- QuantityMeasurement/QuantityMeasurement.cs
+++ QuantityMeasurement/QuantityMeasurement.cs
@@ -1,8 +1,67 @@
-﻿using System;
+﻿// <copyright file="QuantityMeasurement.cs" company="BridgeLabz Solution">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
 
 namespace QuantityMeasurement
 {
-    public class Class1
+    /// <summary>
+    /// Class For QuantityMeasurment.
+    /// </summary>
+    public class QuantityMeasurement
     {
+        private readonly int feet;
+        private readonly int inch;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuantityMeasurement"/> class.
+        /// This is for initializing quantity value.
+        /// </summary>
+        /// <param name="feet"> this store feet Value</param>
+        public QuantityMeasurement(int feet, int inch)
+        {
+            this.feet = feet;
+            this.inch = inch;
+        }
+
+        /// <summary>
+        /// Gets function return feet value.
+        /// </summary>
+        public int Feet => this.feet;
+
+        /// <summary>
+        /// Gets function return inche value.
+        /// </summary>
+        public int Inch => this.inch;
+
+        /// <summary>
+        /// This method is used for checking Equality.
+        /// </summary>
+        /// <param name="obj"> referance of Quantity Measurment</param>
+        /// <returns> true or false.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// For checking hansh code.
+        /// </summary>
+        /// <returns>return int value</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.feet, this.Feet);
+        }
     }
 }
--- QuantityMeasurementTest/QuantityMeasurmentTests.cs
+++ QuantityMeasurementTest/QuantityMeasurmentTests.cs
@@ -4,21 +4,80 @@
 namespace QuantityMeasurmentTest
 {
     using NUnit.Framework;
+    using QuantityMeasurement;
 
     /// <summary>
     /// This class is for testing  QuantityMeasurment.
     /// </summary>
     public class QuantityMeasurmentTests
     {
-        [SetUp]
-        public void Setup()
+        /// <summary>
+        /// Here we place common code required in each test method.
+        /// </summary>
+        private QuantityMeasurement quantityMeasurement;
+        private QuantityMeasurement quantityMeasurement2;
+
+        /// <summary>
+        /// function for testing 0 feet equal to Zero.
+        /// </summary>
+        [Test]
+        public void GivenFeetValueAsZero_WhenEqual_ShouldReturnTrue()
         {
+            this.quantityMeasurement = new QuantityMeasurement(0, 0);
+            this.quantityMeasurement2 = new QuantityMeasurement(0, 0);
+            Assert.AreEqual(this.quantityMeasurement.Feet, this.quantityMeasurement2.Feet);
         }
 
+        /// <summary>
+        /// Testin if object is equal.
+        /// </summary>
         [Test]
-        public void Test1()
+        public void GivenQuantityMeasurementObject_WhenEqual_ShouldReturnTrue()
         {
-            Assert.Pass();
+            bool result = this.quantityMeasurement.Equals(this.quantityMeasurement);
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Testin if object is equal.
+        /// </summary>
+        [Test]
+        public void GivenQuantityMeasurementObject_WhenNotEqual_ShouldReturnFalse()
+        {
+            bool result = this.quantityMeasurement.Equals(this.quantityMeasurement2);
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// Testin if object is null.
+        /// </summary>
+        [Test]
+        public void GivenQuantityMeasurementObject_WhenNull_ShouldReturnFalse()
+        {
+            bool result = this.quantityMeasurement.Equals(null);
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// Testing if object is null.
+        /// </summary>
+        [Test]
+        public void GivenQuantityMeasurementObject_WhenTypeNotEqual_ShouldReturnFalse()
+        {
+            QuantityMeasurmentTests quantityMeasurmentTests = new QuantityMeasurmentTests();
+            bool result = this.quantityMeasurement.Equals(quantityMeasurmentTests);
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// Testing if 0 inch is equal to 0 inch.
+        /// </summary>
+        [Test]
+        public void GivenInchValueZero_WhenTypeNotEqual_ShouldReturnFalse()
+        {
+            this.quantityMeasurement = new QuantityMeasurement(0, 0);
+            this.quantityMeasurement2 = new QuantityMeasurement(0, 0);
+            Assert.AreEqual(this.quantityMeasurement.Inch, this.quantityMeasurement2.Inch);
         }
     }
 
